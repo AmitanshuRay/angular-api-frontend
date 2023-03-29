04d678967e8426b6fbc66aa5b9b7c0dd07b35bd7
Amitanshu Ray <amitanshuray@gmail.com>
2023-03-29 16:45:58 +0530

    Update app.js

--- deploy/app.js
+++ deploy/app.js
@@ -4,7 +4,7 @@ app.get("/test", (req, resp) => {
   resp.send('<h1>Hello World</h1>');
 });
 
-app.use(express.static("/home/amitanshu/Desktop/Server Deploy/angular-server"));
+app.use(express.static("./angular-server"));
 
 app.listen(4200,()=>{
   console.log("Node server is running on http://localhost:4200");
